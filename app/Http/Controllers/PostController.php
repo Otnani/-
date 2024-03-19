20c3fc5d3b445d12989d589751e26d6742b1c4c3
Otani <ootaniayumu297@gmail.com>
2024-03-19 14:46:57 +0900

    カリキュラム８−２

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+
 class PostController extends Controller
 {
-public function index(Post $post)
-  {
-    return $post->get();
-  }
+    public function index(Post $post)
+   {
+       return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
+   }
 }
